20bb2218790a50247427ac8848488fc0a7df1a40
Leithen <leithen113@gmail.com>
2021-11-23 15:43:46 -0700

    Created outline for tables and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO department (name)
+VALUES
+    ('sales'),
+    ('design'),
+    ('manufacturing');
+
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ('manager', 80000, 1),
+    ('manager', 70000, 2),
+    ('manager', 60000, 3),
+    ('sales_associate', 30000, 1),
+    ('designer', 40000, 2),
+    ('machine_operator', 25000, 3),
+    ('customer_service', 30000, 1),
+    ('engineer', 50000, 2),
+    ('maintenance', 30000, 3);
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Kevin', 'Taylor', 1, null),
+    ('Owen', 'Dyer', 2, null),
+    ('Faith', 'North', 3, null),
+    ('Kylie', 'Ross', 4, 1),
+    ('Keith', 'Lyman', 5, 2),
+    ('Paul', 'White', 6, 3),
+    ('Yvonne', 'Henderson', 7, 1),
+    ('Andrew', 'Reid', 8, 2),
+    ('Phil', 'Jones', 9, 3);
