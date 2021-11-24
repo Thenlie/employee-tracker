6ab49e210a98c974a613fbd57b20656afec5a47b
Leithen <leithen113@gmail.com>
2021-11-24 07:35:50 -0700

    Continuing work on prompts

--- script.js
+++ script.js
@@ -3,16 +3,67 @@ const inquirer = require('inquirer');
 
 const initPrompt = () => {
     return inquirer 
-        .prompt([{
+        .prompt({
             type: 'list',
             name: 'action',
             message: 'What would you like to do?',
             choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
-        }])
+        })
     .then((ans) => {
         return ans;
     })
 };
+
+const addDept = () => {
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'deptName',
+            message: 'What is the name of the department?'
+        }])  
+        .then((ans) => {
+            return (ans);
+        })  
+};
+
+const addRole = () => {
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'roleName',
+            message: 'What is the name of the role?'
+            }, {
+            type: 'input',
+            name: 'roleSalary',
+            message: 'What is the salary of the role?'
+            }, {
+            type: 'input',
+            name: 'roleDept',
+            message: 'What department does the role belong to?'
+            }])
+            .then((ans) => {
+                return ans;
+            })    
+};
+
+const addEmployee = () => {
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'addEmployee',
+            message: ''
+        }])    
+};
+
+const updateEmployee = () => {
+    return inquirer
+        .prompt([{
+            type: 'input',
+            name: 'updateEmployee',
+            message: ''
+        }])    
+};
+
 
 initPrompt()
     .then(ans => {
@@ -20,23 +71,27 @@ initPrompt()
             case 'view all departments':
                 console.log(1);
                 return;
-            case 'view all roles' :
+            case 'view all roles':
                 console.log(2);
                 return;
-            case 'view all employees' :
+            case 'view all employees':
                 console.log(3);
                 return;
-            case 'add a department' :
+            case 'add a department':
                 console.log(4);
+                addDept();
                 return;
-            case 'add a role' :
+            case 'add a role':
                 console.log(5);
+                addRole();
                 return;
-            case 'add an employee' :
+            case 'add an employee':
                 console.log(6);
+                addEmployee();
                 return;
-            case 'update an employee role' :
+            case 'update an employee role':
                 console.log(7);
+                updateEmployee();
                 return;
         }
         console.log(ans.action);
